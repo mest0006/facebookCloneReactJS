f2aa9dc35f4e1204b607555af3b4f5779a88fedd
Borbala Mester <46438846+mest0006@users.noreply.github.com>
2021-03-13 09:57:10 -0500

    facebookcone

---
+++ facebook-clone/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Feed from './Feed'
+
+function App () {
+  return (
+    <div className="app">
+
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+
+        {/** wigets */}
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
